b9488ee83872187c3868698269d16ad06845bd4c
Dylan Banks <dylanbanks96@users.noreply.github.com>
2025-09-26 14:24:34 +0100

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/company-website-template/',
+  base: '/zermatt-template/',
   plugins: [
     vue(),
     vueDevTools(),
