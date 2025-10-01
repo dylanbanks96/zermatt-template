00844b19b899d05dec4efcef1ca401d548413abb
Dylan Banks <dylanbanks96@users.noreply.github.com>
2025-10-01 15:35:35 +0100

    Add github link to footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,11 +1,17 @@
 <template>
   <footer class="footer">
     <div class="container">
+      <div class="github-link">
+        <img :src="githublogo" alt="GitHub Logo">
+        <a href="https://github.com/dylanbanks96/zermatt-template" target="_blank" rel="noopener noreferrer">View on
+          GitHub</a>
+      </div>
       <p>© {{ year }} Zermatt — Precision at Every Turn</p>
     </div>
   </footer>
 </template>
 
 <script setup>
+import githublogo from '@/assets/github-mark.png'
 const year = new Date().getFullYear()
 </script>
